4a29189f3005009858962991c0162ae51e051722
Hélène <h.quoitot@gmail.com>
2022-12-12 20:13:32 +0100

    Add flip loading component

---
+++ src/components/common/loading/FlipLoading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './FlipLoading.scss';
+
+const FlipLoading = () => (
+    <div className="flip-loading">
+        <div />
+        <div />
+    </div>
+);
+
+export default FlipLoading;
