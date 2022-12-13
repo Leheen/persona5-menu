483d4b8c75b7d53b2394f30cc9615ee3ba8ffa45
Hélène <h.quoitot@gmail.com>
2022-12-13 21:43:26 +0100

    Add spotlight loading component

---
+++ src/components/common/loading/SpotlightLoading/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './SpotlightLoading.scss';
+
+const SpotlightLoading = () => (
+    <div className="spotlight-loading">
+        <div />
+        <div />
+        <div />
+    </div>
+);
+
+export default SpotlightLoading;
